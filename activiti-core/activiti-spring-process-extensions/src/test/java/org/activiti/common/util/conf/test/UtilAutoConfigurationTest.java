ad30e659db8aca5bab773379a0dcf9eb99f831ad
Gian Carlo Pace <giancarlo.pace@gmail.com>
2025-06-06 00:08:46 +0200

    refactor: remove unused activiti-common-util module from pom.xml

--- activiti-core/activiti-spring-process-extensions/src/test/java/org/activiti/common/util/conf/test/UtilAutoConfigurationTest.java
+++ activiti-core/activiti-spring-process-extensions/src/test/java/org/activiti/common/util/conf/test/UtilAutoConfigurationTest.java
@@ -22,7 +22,6 @@ import org.activiti.common.util.conf.ActivitiCoreCommonUtilAutoConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
